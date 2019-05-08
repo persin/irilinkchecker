4541db5f091435d4070a717d4d34696c232c0f23
ali talebi <talebi.ali.prv@gmail.com>
2019-05-08 23:34:09 +0430

    first commit v0.6

---
+++ pages/popup/popup.js
@@ -0,0 +1,9 @@
+var listButton = document.getElementById('list-button');
+listButton.addEventListener('click',() => {
+    chrome.tabs.create({ url: '/pages/listpage/listpage.html' })
+})
+
+var tutButton = document.getElementById('tut-button');
+tutButton.addEventListener('click',() => {
+    chrome.tabs.create({ url: '/pages/tutorial/index.html' })
+})
