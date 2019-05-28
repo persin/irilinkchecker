fd00c9363a13e29b290b1d15e72a0c91b2dd8f6b
Ali Talebi <talebi.ali.prv@gmail.com>
2019-05-28 10:36:43 +0430

    add share, support and donate button to popup (v0.70)

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -9,3 +9,33 @@ tutButton.addEventListener('click',() => {
     // chrome.tabs.create({ url: '/pages/tutorial/index.html' })
     chrome.runtime.sendMessage ({ "open": '/pages/listpage/index.html' })
 })
+
+var rateButton = document.getElementById('rate-button');
+rateButton.addEventListener('click',() => {
+    chrome.tabs.create({ url: 'https://chrome.google.com/webstore/detail/iri-link-checker/foakpniabhngjaebkipgllinnnbmemee' })
+})
+
+var donateButton = document.getElementById('donate-button');
+donateButton.addEventListener('click',() => {
+    chrome.tabs.create({ url: 'https://idpay.ir/irilinkchecker' })
+})
+
+var githubButton = document.getElementById('github-button');
+githubButton.addEventListener('click',() => {
+    chrome.tabs.create({ url: 'https://github.com/alitalebiuix/irilinkchecker' })
+})
+
+
+var shareButton = document.getElementById('share-button');
+shareButton.addEventListener('click',() => {
+    chrome.tabs.create({ url: `mailto:ایمیل را وارد کنید?subject=افزونه تشخیص سایت‌های نیم‌بهای داخلی&body=با این افزونه میشه براحتی از نیم‌بها بودن سایت در حال مشاهده آگاه شد.%0d%0a
+    لینک افزونه%0d%0a
+    http://yon.ir/iranilink%0d%0a
+    ویدئوی آموزش افزونه%0d%0a
+    https://www.aparat.com/v/Z6olk` })
+})
+
+var supportButton = document.getElementById('support-button');
+supportButton.addEventListener('click',() => {
+    chrome.tabs.create({ url: 'mailto://talebi.ali.prv@gmail.com' })
+})
